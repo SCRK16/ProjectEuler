a5bc8c59db912297dd66034f5b79644f26c51ded
SCRK16 <rick.koenders@hotmail.com>
2019-06-13 22:52:08 +0200

    Solved 34

---
+++ src/1-100/34.py
@@ -0,0 +1,23 @@
+def factorial(n, memory):
+    if n in memory:
+        return memory[n]
+    memory[n] = n * factorial(n-1, memory)
+    return memory[n]
+
+result = 0
+memory = {0: 1, 1: 1}
+
+for i in range(3, 1000000):
+    digits = [int(x) for x in str(i)]
+    factorials = [factorial(x, memory) for x in digits]
+    if sum(factorials) == i:
+        result += i
+
+print(result)
+
+
+"""
+Runtime without memory: 6.095s
+Runtime with memory:    2.910s
+Reducing search space from 1,000,000 to 100,000 reduces time to: 0.292s
+"""
