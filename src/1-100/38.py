3655f616b36da71d8b94bd0e07de0f80f4cf583a
SCRK16 <rick.koenders@hotmail.com>
2019-06-14 14:25:02 +0200

    Solved 38

---
+++ src/1-100/38.py
@@ -0,0 +1,24 @@
+def num_digits(n):
+    return len(str(n))
+
+def concatenate(n, m):
+    return int(''.join([str(n), str(m)]))
+
+def is_pandigital(n):
+    if not 99999999 <= n < 999999999:
+        return False
+    digits = {int(i) for i in str(n)}
+    return digits == {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+best = 0
+
+for i in range(1, 10000):
+    current = i
+    mult = 2
+    while num_digits(current) < 9:
+        current = concatenate(current, i * mult)
+        mult += 1
+    if is_pandigital(current) and current > best:
+        best = current
+
+print(best)
